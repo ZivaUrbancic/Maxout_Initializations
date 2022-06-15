266b93d724fdbe1b34a676e0d592c769b0c1ac00
Iolo Jones <73426726+Iolo-Jones@users.noreply.github.com>
2022-06-15 12:39:56 +0200

    Fixing log class errors

--- log_classes.py
+++ log_classes.py
@@ -3,28 +3,30 @@ import copy
 # Create a RunLog for each run, which is updated with the cost vector,
 # loss and accuracy, and experiment number.
 
-# The class Log gathers the runlogs together in a big dictionary.
-# This is used in each experiment file to collect multiple runs,
-# as well as to append Logs together.
+_datasets = ['mnist', 'cifar', 'speechcommands']
+_sizes = ['small', 'large']
+_ranks = ['relu', 'maxout2', 'maxout3', 'maxout5']
 
 class RunLog:
     
     def __init__(self, dataset, size, rank, 
                  adjust_regions, adjust_variance):
+        
         self.dataset = dataset
         self.size = size
         self.rank = rank
+        
+        self.experiment_number = 0
         self.adjust_regions = adjust_regions
         self.adjust_variance = adjust_variance
         self.cost_vector = None
-        self.experiment_number = 0
         self.loss = []
         self.accuracy = []
         
     # If the cost vector is being saved to the log L then
     # write l.cost_vector = ... else leave as None
     
-    # Record the experiment number as l.experiement_number = ...
+    # Record the experiment number as l.experiment_number = ...
     
     # Use the following two functions to record the loss
     # and accuracy at a given epoch and timestep.
@@ -33,21 +35,24 @@ class RunLog:
         self.loss.append([epoch, step, loss])
         
     def record_accuracy(self, epoch, step, acc):
-        self.loss.append([epoch, step, acc])
-        
+        self.accuracy.append([epoch, step, acc])
+
+# The class Log gathers the runlogs together in a big dictionary.
+# This is used in each experiment file to collect multiple runs,
+# as well as to append Logs together.
+
 class Log:
     
-    def __init__(self, ranks = ['relu',
-                                      'maxout2',
-                                      'maxout3',
-                                      'maxout5']):
-        self.ranks = ranks
-        ranks_dict = {a : [] for a in ranks}
-        sizes = {s : copy.deepcopy(ranks_dict) for s in ['small',
-                                            'large']}
-        self.dict = {d : copy.deepcopy(sizes) for d in ['mnist',
-                                                'cifar',
-                                                'speechcommands']}
+    def __init__(self, dictionary = None, ranks = _ranks):
+        
+        if dictionary is None:
+            self.ranks = ranks
+            ranks_dict = {a : [] for a in ranks}
+            sizes = {s : copy.deepcopy(ranks_dict) for s in _sizes}
+            self.dict = {d : copy.deepcopy(sizes) for d in _datasets}
+        else:
+            self.ranks = [key for key in dictionary['mnist']['small']]
+            self.dict = dictionary
         
     def add_runlog(self, runlog):
         directory = self.dict[runlog.dataset][runlog.size][runlog.rank]
@@ -68,17 +73,19 @@ class Log:
                    file=open(str(experiment_number)+".log",'+a'))
             
     def append(self, log):
-        for dataset in ['mnist', 'cifar', 'speechcommands']:
-            for size in ['small', 'large']:
-                for rank in self.ranks:
+        for dataset in _datasets:
+            for size in _sizes:
+                for rank in _ranks:
                     current = self.dict[dataset][size][rank]
-                    update = log.dict[dataset][size][rank]
-                    if update.experiement_number in [c.experiement_number
-                                                     for c in current]:
-                        print('This run of', dataset, size, rank,
-                              'has already been added to this log')
-                    else:
-                        current.append(update)
+                    updates = log.dict[dataset][size][rank]
+                    for update in updates:
+                        if update['experiment_number'] in [c['experiment_number']
+                                                           for c in current]:
+                            print('This run of', dataset, size, rank,
+                                      'has already been added to this log')
+                        else:
+                            current.append(update)
+
         
         
         
