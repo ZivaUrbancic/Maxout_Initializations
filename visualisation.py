9dddbadd7dbec1c6ce67516a5630052ef0389dc3
Yue Ren <yue.ren.kl@gmail.com>
2023-02-16 11:43:44 +0000

    new: visualization scripts + old folder

--- visualisation.py
+++ visualisation.py
@@ -1,111 +1,149 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon May  2 08:44:14 2022
-
-@author: ziva
-"""
-
 import numpy as np
+import scipy as sp
 import matplotlib.pyplot as plt
 
-def read_data_from_exp(file, num_runs, num_epochs):
-    X = np.zeros((num_runs, 60*num_epochs))
-    f = open(file, "r")
-    for i, l in enumerate(f):
-        a, b = l.split("], [")
-        a = a[2:].split(", ")
-        b = b[:-3].split(", ")
-        n_run = int(a[0])
-        n_epoch = int(a[1])
-        n_entry = (int(a[2])+1)/10 + n_epoch*60
-        n_entry = int(n_entry - 1)
-        X[n_run, n_entry] = float(b[0])
-    return X
+import json
 
-def compute_Y1_Y2(experiment_number, acc_or_loss, num_runs, num_epochs):
-    f1 = experiment_number+"_"+acc_or_loss+"_A.log"
-    f2 = experiment_number+"_"+acc_or_loss+"_B.log"
-    D = read_data_from_exp(f1, num_runs, num_epochs)
-    R = read_data_from_exp(f2, num_runs, num_epochs)
-    return D, R
+exec(open("../log_classes.py").read())
 
-def compute_Y1_Y2_Y3(experiment_number, acc_or_loss, num_runs, num_epochs):
-    f1 = experiment_number+"_"+acc_or_loss+"_A.log"
-    f2 = experiment_number+"_"+acc_or_loss+"_B.log"
-    f3 = experiment_number+"_"+acc_or_loss+"_C.log"
-    Y1 = read_data_from_exp(f1, num_runs, num_epochs)
-    Y2 = read_data_from_exp(f2, num_runs, num_epochs)
-    Y3 = read_data_from_exp(f3, num_runs, num_epochs)
-    return Y1, Y2, Y3
+data_log = Log()
 
-def compute_Ys(experiment_number, models, acc_or_loss, num_runs, num_epochs):
-    fs = [experiment_number+"_"+acc_or_loss+"_"+model+".log" for model in models]
-    Ys = [read_data_from_exp(fi, num_runs, num_epochs) for fi in fs]
-    return Ys
+experiment_numbers = [294863938]
 
-def visualiseOld2(Y1, Y2, Y1_std=None, Y2_std=None):
-    X = np.linspace(0, num_epochs, num=len(Y1))
-    plt.plot(X, Y1, "royalblue")
-    plt.plot(X, Y2, "orange")
-    if not (Y1_std is None or Y2_std is None):
-        plt.fill_between(X, Y1-Y1_std, Y1+Y1_std, color="skyblue")
-        plt.fill_between(X, Y2-Y2_std, Y2+Y2_std, color="navajowhite")
+for number in experiment_numbers:
+    print('Experiment number:', number)
+    with open(str(number) + '.log') as file:
+        file_string = file.read()
+        file_string = file_string.replace("'", "\"")
+        file_string = file_string.replace("True", "true")
+        file_string = file_string.replace("False", "false")
+    file_log = Log(json.loads(file_string))
+    data_log.append(file_log)
 
-def visualiseOld3(Y1, Y2, Y3, Y1_std=None, Y2_std=None, Y3_std=None):
-    X = np.linspace(0, num_epochs, num=len(Y1))
-    plt.plot(X, Y1, "r")
-    plt.plot(X, Y2, "b")
-    plt.plot(X, Y3, "g")
-    if not (Y1_std is None or Y2_std is None or Y3_std is None):
-        plt.fill_between(X, Y1-Y1_std, Y1+Y1_std, color="skyblue")
-        plt.fill_between(X, Y2-Y2_std, Y2+Y2_std, color="navajowhite")
-        plt.fill_between(X, Y3-Y3_std, Y3+Y3_std, color="navajowhite")
+init_std = []
+init_var = []
+init_full = []
 
-def visualise(Ys, Ys_std=None):
-    X = np.linspace(0, num_epochs, num=len(Ys[1]))
-    colours = ["red","blue","green","darkorange"]
-    colours_std = ["mistyrose","lightskyblue","palegreen","bisque"]
-
-    for Yi,colour in zip(Ys,colours):
-        plt.plot(X, Yi, colour)
-
-    if Ys_std != None:
-        for Yi,Yi_std,colour_std in zip(Ys,Ys_std,colours_std):
-            plt.fill_between(X, Yi+Yi_std, Yi-Yi_std, color=colour_std)
-
-experiment_numbers = ["533125786"]
-experiment_models = ["A", "B", "C", "D"]
-Ys = [[] for model in experiment_models] # accuracies
-Zs = [[] for model in experiment_models] # losses
-for experiment_number in experiment_numbers:
-
-    f = open(experiment_number+".log", "r")
-    for i, l in enumerate(f):
-        if i == 6:
-            _, s = l.split(": ")
-            num_runs = int(s.strip())
-        elif i == 7:
-            s = l.split(" ")
-            num_epochs = int(s[2])
-
-    ys = compute_Ys(experiment_number, experiment_models, "acc", num_runs, num_epochs)
-    zs = compute_Ys(experiment_number, experiment_models, "loss", num_runs, num_epochs)
-    if len(Ys[0])==0:
-        Ys = [np.array(yi) for yi in ys]
-        Zs = [np.array(zi) for zi in zs]
+for training in data_log.dict['mnist']['small']['relu']:
+    if not training['adjust_variance']:
+        init_std.append(training)
+    elif training['adjust_regions']:
+        init_full.append(training)
     else:
-        for i in range(len(Ys)):
-            Ys[i] = np.concatenate((Ys[i], np.array(ys[i])), axis = 0)
-            Zs[i] = np.concatenate((Zs[i], np.array(zs[i])), axis = 0)
+        init_var.append(training)
 
-Ys_std = [np.array(Yi).std(axis=0) for Yi in Ys]
-Zs_std = [np.array(Zi).std(axis=0) for Zi in Zs]
-Ys = [Yi.mean(axis=0) for Yi in Ys]
-Zs = [Zi.mean(axis=0) for Zi in Zs]
+cost_vectors_std = [run['cost_vectors'] for run in init_std]
+cost_vectors_var = [run['cost_vectors'] for run in init_var]
+cost_vectors_full = [run['cost_vectors'] for run in init_full]
 
-plt.figure(0)
-visualise(Ys)#, Ys_std)
-plt.figure(1)
-visualise(Zs)#, Zs_std)
+losses_std = [run['losses'] for run in init_std]
+losses_var = [run['losses'] for run in init_var]
+losses_full = [run['losses'] for run in init_full]
+
+accuracies_std = [run['accuracies'] for run in init_std]
+accuracies_var = [run['accuracies'] for run in init_var]
+accuracies_full = [run['accuracies'] for run in init_full]
+
+def epoch_and_step_to_partial_epoch(runs):
+    new_list = []
+    for run in runs:
+        run_list = []
+        for epoch, step, value in run:
+            partial_epoch = epoch + step/600
+            run_list.append([partial_epoch, value])
+        new_list.append(np.array(run_list))
+    return new_list
+
+def epoch_and_step_to_partial_epoch_accuracies(runs):
+    new_list = []
+    for run in runs:
+        run_list = []
+        for epoch, step, value in run:
+            partial_epoch = epoch + step/600
+            run_list.append([partial_epoch, value[0]])
+        new_list.append(np.array(run_list))
+    return new_list
+
+losses_std = epoch_and_step_to_partial_epoch(losses_std)
+losses_var = epoch_and_step_to_partial_epoch(losses_var)
+losses_full = epoch_and_step_to_partial_epoch(losses_full)
+
+accuracies_std = epoch_and_step_to_partial_epoch_accuracies(accuracies_std)
+accuracies_var = epoch_and_step_to_partial_epoch_accuracies(accuracies_var)
+accuracies_full = epoch_and_step_to_partial_epoch_accuracies(accuracies_full)
+
+#%%
+
+def compute_average_across_runs(losses_vector):
+
+    average_across_runs = []
+    n_runs = len(losses_vector)
+    n_steps = len(losses_vector[0])
+    for step in range(n_steps):
+        loss = 0
+        for run in range(n_runs):
+            loss += losses_vector[run][step][1]
+        average_across_runs.append([losses_vector[0][step][0],loss/n_runs])
+
+    return average_across_runs
+
+average_loss_std = compute_average_across_runs(losses_std)
+average_loss_var = compute_average_across_runs(losses_var)
+average_loss_full = compute_average_across_runs(losses_full)
+
+average_accuracies_std = compute_average_across_runs(accuracies_std)
+average_accuracies_var = compute_average_across_runs(accuracies_var)
+average_accuracies_full = compute_average_across_runs(accuracies_full)
+
+def compute_average_tcosts_nregions_across_runs(cost_vectors):
+
+    average_tcosts_across_runs = []
+    average_nregions_across_runs = []
+
+    n_runs = len(cost_vectors)
+    n_steps = len(cost_vectors[0])
+    for step in range(n_steps):
+        tcost = 0
+        nregion = 0
+        for run in range(n_runs):
+            region_costs = cost_vectors[run][step][2][2]
+            for region_cost_entry in region_costs:
+                tcost += region_cost_entry[0]*region_cost_entry[2]
+                nregion += region_cost_entry[2]
+
+        average_tcosts_across_runs.append(tcost/n_runs)
+        average_nregions_across_runs.append(nregion/n_runs)
+
+    return average_tcosts_across_runs, average_nregions_across_runs
+
+average_tcosts_std, average_nregions_std = compute_average_tcosts_nregions_across_runs(cost_vectors_std)
+average_tcosts_var, average_nregions_var = compute_average_tcosts_nregions_across_runs(cost_vectors_var)
+average_tcosts_full, average_nregions_full = compute_average_tcosts_nregions_across_runs(cost_vectors_full)
+
+fig, (ax1, ax2, ax3, ax4) = plt.subplots(4)
+
+ax1.plot(range(97),average_tcosts_std)
+ax1.plot(range(97),average_tcosts_var)
+ax1.plot(range(97),average_tcosts_full)
+ax1.legend(['std','var','full'])
+ax1.set_ylabel('total costs')
+
+ax2.plot(range(97),average_nregions_std)
+ax2.plot(range(97),average_nregions_var)
+ax2.plot(range(97),average_nregions_full)
+ax2.legend(['std','var','full'])
+ax2.set_ylabel('number regions')
+
+ax3.plot([i[0] for i in average_loss_std],[i[1] for i in average_loss_std])
+ax3.plot([i[0] for i in average_loss_var],[i[1] for i in average_loss_var])
+ax3.plot([i[0] for i in average_loss_full],[i[1] for i in average_loss_full])
+ax3.legend(['std','var','full'])
+ax3.set_ylabel('loss')
+
+ax4.plot([i[0] for i in average_accuracies_std],[i[1] for i in average_accuracies_std])
+ax4.plot([i[0] for i in average_accuracies_var],[i[1] for i in average_accuracies_var])
+ax4.plot([i[0] for i in average_accuracies_full],[i[1] for i in average_accuracies_full])
+ax4.legend(['std','var','full'])
+ax4.set_ylabel('accuracy')
+
 plt.show()
