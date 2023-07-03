ceeb8c211109242be41743f9f7109202c040de4c
ZivaUrbancic <ziva.urbancic@gmail.com>
2023-07-03 13:41:28 +0100

    chg: removed adjust_variance parameter and commented out model_Rescale

--- relu_experiment.py
+++ relu_experiment.py
@@ -17,8 +17,8 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # Experiment hyperparameters
 ###
 experiment_number = random.randint(0,999999999)
-num_runs = 3
-num_epochs = 2
+num_runs = 24
+num_epochs = 24
 batch_size = 100
 learning_rate = 0.001
 dataset = "MNIST"
@@ -161,10 +161,12 @@ class ReLUNet(nn.Module):
 # Running experiments
 ###
 FileLog = Log()
-ModelList = []
+ModelANetworks = []
+ModelBNetworks = []
+ModelCNetworks = []
 for run in range(num_runs):
     runlogA = RunLog("mnist",network_size,"relu",False,False,experiment_number=experiment_number)
-    runlogB = RunLog("mnist",network_size,"relu",False,True,experiment_number=experiment_number)
+    # runlogB = RunLog("mnist",network_size,"relu",False,True,experiment_number=experiment_number)
     runlogC = RunLog("mnist",network_size,"relu",True,True,experiment_number=experiment_number)
 
     train_loader = torch.utils.data.DataLoader(train_dataset,
@@ -174,46 +176,40 @@ for run in range(num_runs):
     Xtest, Ytest = sample_dataset(test_dataset, test_loader, data_sample_size)
 
     modelDefault = ReLUNet().to(device) # no reinit + batchnorm
-    modelRescale = ReLUNet().to(device) # reinit + batchnorm
+    # modelRescale = ReLUNet().to(device) # reinit + batchnorm
     modelReinit = ReLUNet().to(device) # reinit + our rescaling
 
     c_default = reinitialise_network(modelDefault, X, Y,
                                      return_cost_vector = True,
-                                     adjust_regions = False,
-                                     adjust_variance = False)
+                                     adjust_regions = False)
     modelDefault = modelDefault.to(device)
     c_default_test = reinitialise_network(modelDefault, Xtest, Ytest,
                                           return_cost_vector = True,
-                                          adjust_regions = False,
-                                          adjust_variance = False)
+                                          adjust_regions = False)
     runlogA.record_cost_vector(0,0,[c_default,c_default_test])
 
-    c_rescale = reinitialise_network(modelRescale, X, Y,
-                                     return_cost_vector = True,
-                                     adjust_regions = False,
-                                     adjust_variance = True)
-    modelRescale = modelRescale.to(device)
-    c_rescale_test = reinitialise_network(modelRescale, Xtest, Ytest,
-                                          return_cost_vector = True,
-                                          adjust_regions = False,
-                                          adjust_variance = False)
-    runlogB.record_cost_vector(0,0,[c_rescale,c_rescale_test])
+    # c_rescale = reinitialise_network(modelRescale, X, Y,
+    #                                  return_cost_vector = True,
+    #                                  adjust_regions = False)
+    # modelRescale = modelRescale.to(device)
+    # c_rescale_test = reinitialise_network(modelRescale, Xtest, Ytest,
+    #                                       return_cost_vector = True,
+    #                                       adjust_regions = False)
+    # runlogB.record_cost_vector(0,0,[c_rescale,c_rescale_test])
 
     print("run ",run+1," of ",num_runs,": reinitialising")
     c_reinit = reinitialise_network(modelReinit, X, Y,
                                     return_cost_vector=True,
-                                    adjust_regions = True,
-                                    adjust_variance = True)
+                                    adjust_regions = True)
     modelReinit = modelReinit.to(device)
     c_reinit_test = reinitialise_network(modelReinit, Xtest, Ytest,
                                          return_cost_vector = True,
-                                         adjust_regions = False,
-                                         adjust_variance = False)
+                                         adjust_regions = False)
     runlogC.record_cost_vector(0,0,[c_reinit,c_reinit_test])
 
     criterion = nn.CrossEntropyLoss()
     optimizerDefault = torch.optim.Adam(modelDefault.parameters())
-    optimizerRescale = torch.optim.Adam(modelRescale.parameters())
+    # optimizerRescale = torch.optim.Adam(modelRescale.parameters())
     optimizerReinit = torch.optim.Adam(modelReinit.parameters())
 
     n_total_steps = len(train_loader)
@@ -227,9 +223,10 @@ for run in range(num_runs):
     imagesTest = imagesTest.to(device)
     labelsTest = labelsTest.to(device)
 
-    modelListInit = [deepcopy_network(modelDefault),
-                     deepcopy_network(modelRescale),
-                     deepcopy_network(modelReinit)]
+    ModelARunNetworks = [deepcopy_network(modelDefault)]
+    # ModelBRunNetworks = [deepcopy_network(modelRescale)]
+    ModelCRunNetworks = [deepcopy_network(modelReinit)]
+    
 
     for epoch in range(num_epochs):
         for i, (images, labels) in enumerate(train_loader):
@@ -238,15 +235,15 @@ for run in range(num_runs):
             # Forward pass
             outputsDefault = modelDefault(images)
             lossDefault = criterion(outputsDefault, labels)
-            outputsRescale = modelRescale(images)
-            lossRescale = criterion(outputsRescale, labels)
+            # outputsRescale = modelRescale(images)
+            # lossRescale = criterion(outputsRescale, labels)
             outputsReinit = modelReinit(images)
             lossReinit = criterion(outputsReinit, labels)
 
             outputsDefaultTest = modelDefault(imagesTest)
             lossDefaultTest = criterion(outputsDefaultTest, labelsTest)
-            outputsRescaleTest = modelRescale(imagesTest)
-            lossRescaleTest = criterion(outputsRescaleTest, labelsTest)
+            # outputsRescaleTest = modelRescale(imagesTest)
+            # lossRescaleTest = criterion(outputsRescaleTest, labelsTest)
             outputsReinitTest = modelReinit(imagesTest)
             lossReinitTest = criterion(outputsReinitTest, labelsTest)
 
@@ -254,9 +251,9 @@ for run in range(num_runs):
             optimizerDefault.zero_grad()
             lossDefault.backward()
             optimizerDefault.step()
-            optimizerRescale.zero_grad()
-            lossRescale.backward()
-            optimizerRescale.step()
+            # optimizerRescale.zero_grad()
+            # lossRescale.backward()
+            # optimizerRescale.step()
             optimizerReinit.zero_grad()
             lossReinit.backward()
             optimizerReinit.step()
@@ -271,25 +268,25 @@ for run in range(num_runs):
                 ###
                 with torch.no_grad():
                     n_correct_default = 0
-                    n_correct_rescale = 0
+                    # n_correct_rescale = 0
                     n_correct_reinit = 0
                     n_samples = 0
                     n_class_correct_default = [0 for i in range(10)]
-                    n_class_correct_rescale = [0 for i in range(10)]
+                    # n_class_correct_rescale = [0 for i in range(10)]
                     n_class_correct_reinit = [0 for i in range(10)]
                     n_class_samples = [0 for i in range(10)]
                     for images, labels in train_loader:
                         images = images.to(device)
                         labels = labels.to(device)
                         outputsDefault = modelDefault(images)
-                        outputsRescale = modelRescale(images)
+                        # outputsRescale = modelRescale(images)
                         outputsReinit = modelReinit(images)
                         _, predictedDefault = torch.max(outputsDefault, 1)
-                        _, predictedRescale = torch.max(outputsRescale, 1)
+                        # _, predictedRescale = torch.max(outputsRescale, 1)
                         _, predictedReinit = torch.max(outputsReinit, 1)
                         n_samples += labels.size(0)
                         n_correct_default += (predictedDefault == labels).sum().item()
-                        n_correct_rescale += (predictedRescale == labels).sum().item()
+                        # n_correct_rescale += (predictedRescale == labels).sum().item()
                         n_correct_reinit += (predictedReinit == labels).sum().item()
 
                         for j in range(labels.size(0)):
@@ -297,42 +294,42 @@ for run in range(num_runs):
                             pred_default = predictedDefault[j]
                             if (label == pred_default):
                                 n_class_correct_default[label] += 1
-                            pred_rescale = predictedRescale[j]
-                            if (label == pred_rescale):
-                                n_class_correct_rescale[label] += 1
+                            # pred_rescale = predictedRescale[j]
+                            # if (label == pred_rescale):
+                            #     n_class_correct_rescale[label] += 1
                             pred_reinit = predictedReinit[j]
                             if (label == pred_reinit):
                                 n_class_correct_reinit[label] += 1
                             n_class_samples[label] += 1
 
                     acc_default = [round(n_correct_default / n_samples,3)]
-                    acc_rescale = [round(n_correct_rescale / n_samples,3)]
+                    # acc_rescale = [round(n_correct_rescale / n_samples,3)]
                     acc_reinit = [round(n_correct_reinit / n_samples,3)]
                     acc_default += [round(n_class_correct_default[j] / n_class_samples[j],3) for j in range(10)]
-                    acc_rescale += [round(n_class_correct_rescale[j] / n_class_samples[j],3) for j in range(10)]
+                    # acc_rescale += [round(n_class_correct_rescale[j] / n_class_samples[j],3) for j in range(10)]
                     acc_reinit += [round(n_class_correct_reinit[j] / n_class_samples[j],3) for j in range(10)]
 
                 with torch.no_grad():
                     n_correct_default = 0
-                    n_correct_rescale = 0
+                    # n_correct_rescale = 0
                     n_correct_reinit = 0
                     n_samples = 0
                     n_class_correct_default = [0 for i in range(10)]
-                    n_class_correct_rescale = [0 for i in range(10)]
+                    # n_class_correct_rescale = [0 for i in range(10)]
                     n_class_correct_reinit = [0 for i in range(10)]
                     n_class_samples = [0 for i in range(10)]
                     for images, labels in test_loader:
                         images = images.to(device)
                         labels = labels.to(device)
                         outputsDefault = modelDefault(images)
-                        outputsRescale = modelRescale(images)
+                        # outputsRescale = modelRescale(images)
                         outputsReinit = modelReinit(images)
                         _, predictedDefault = torch.max(outputsDefault, 1)
-                        _, predictedRescale = torch.max(outputsRescale, 1)
+                        # _, predictedRescale = torch.max(outputsRescale, 1)
                         _, predictedReinit = torch.max(outputsReinit, 1)
                         n_samples += labels.size(0)
                         n_correct_default += (predictedDefault == labels).sum().item()
-                        n_correct_rescale += (predictedRescale == labels).sum().item()
+                        # n_correct_rescale += (predictedRescale == labels).sum().item()
                         n_correct_reinit += (predictedReinit == labels).sum().item()
 
                         for j in range(labels.size(0)):
@@ -340,26 +337,26 @@ for run in range(num_runs):
                             pred_default = predictedDefault[j]
                             if (label == pred_default):
                                 n_class_correct_default[label] += 1
-                            pred_rescale = predictedRescale[j]
-                            if (label == pred_rescale):
-                                n_class_correct_rescale[label] += 1
+                            # pred_rescale = predictedRescale[j]
+                            # if (label == pred_rescale):
+                            #     n_class_correct_rescale[label] += 1
                             pred_reinit = predictedReinit[j]
                             if (label == pred_reinit):
                                 n_class_correct_reinit[label] += 1
                             n_class_samples[label] += 1
 
                     acc_default_test = [round(n_correct_default / n_samples,3)]
-                    acc_rescale_test = [round(n_correct_rescale / n_samples,3)]
+                    # acc_rescale_test = [round(n_correct_rescale / n_samples,3)]
                     acc_reinit_test = [round(n_correct_reinit / n_samples,3)]
                     acc_default_test += [round(n_class_correct_default[j] / n_class_samples[j],3) for j in range(10)]
-                    acc_rescale_test += [round(n_class_correct_rescale[j] / n_class_samples[j],3) for j in range(10)]
+                    # acc_rescale_test += [round(n_class_correct_rescale[j] / n_class_samples[j],3) for j in range(10)]
                     acc_reinit_test += [round(n_class_correct_reinit[j] / n_class_samples[j],3) for j in range(10)]
 
                     runlogA.record_accuracies(epoch,i+1,[acc_default,acc_default_test])
-                    runlogB.record_accuracies(epoch,i+1,[acc_rescale,acc_rescale_test])
+                    # runlogB.record_accuracies(epoch,i+1,[acc_rescale,acc_rescale_test])
                     runlogC.record_accuracies(epoch,i+1,[acc_reinit,acc_reinit_test])
                     runlogA.record_losses(epoch,i+1,[round(lossDefault.item(),3),round(lossDefaultTest.item(),3)])
-                    runlogB.record_losses(epoch,i+1,[round(lossRescale.item(),3),round(lossRescaleTest.item(),3)])
+                    # runlogB.record_losses(epoch,i+1,[round(lossRescale.item(),3),round(lossRescaleTest.item(),3)])
                     runlogC.record_losses(epoch,i+1,[round(lossReinit.item(),3),round(lossReinitTest.item(),3)])
 
             ###
@@ -370,27 +367,26 @@ for run in range(num_runs):
                 print("    logging linear regions at step ",i+1)
 
                 cost_default = reinitialise_network(modelDefault, X, Y, True, False, False)
-                cost_rescale = reinitialise_network(modelRescale, X, Y, True, False, False)
+                # cost_rescale = reinitialise_network(modelRescale, X, Y, True, False, False)
                 cost_reinit = reinitialise_network(modelReinit, X, Y, True, False, False)
                 cost_default_test = reinitialise_network(modelDefault, Xtest, Ytest, True, False, False)
-                cost_rescale_test = reinitialise_network(modelRescale, Xtest, Ytest, True, False, False)
+                # cost_rescale_test = reinitialise_network(modelRescale, Xtest, Ytest, True, False, False)
                 cost_reinit_test = reinitialise_network(modelReinit, Xtest, Ytest, True, False, False)
                 runlogA.record_cost_vector(epoch,i+1,[cost_default,cost_default_test])
-                runlogB.record_cost_vector(epoch,i+1,[cost_rescale,cost_rescale_test])
+                # runlogB.record_cost_vector(epoch,i+1,[cost_rescale,cost_rescale_test])
                 runlogC.record_cost_vector(epoch,i+1,[cost_reinit,cost_reinit_test])
-
-    modelListTrained = [deepcopy_network(modelDefault),
-                        deepcopy_network(modelRescale),
-                        deepcopy_network(modelReinit)]
+                ModelARunNetworks.append(deepcopy_network(modelDefault))
+                # ModelBRunNetworks.append(deepcopy_network(modelRescale))
+                ModelCRunNetworks.append(deepcopy_network(modelReinit))
 
     FileLog.add_runlog(runlogA)
-    FileLog.add_runlog(runlogB)
+    # FileLog.add_runlog(runlogB)
     FileLog.add_runlog(runlogC)
-
-    modelList.append([[modelListInit[0],modelListTrained[0]],
-                      [modelListInit[1],modelListTrained[1]],
-                      [modelListInit[2],modelListTrained[2]]])
+    ModelANetworks.append(ModelARunNetworks)
+    # ModelBNetworks.append(ModelBRunNetworks)
+    ModelCNetworks.append(ModelCRunNetworks)
 
 FileLog.save(experiment_number)
-torch.save(modelList,str(experiment_number)+'.pt')
+#torch.save([ModelANetworks, ModelBNetworks, ModelCNetworks],str(experiment_number)+'.pt')
+torch.save([ModelANetworks, ModelCNetworks],str(experiment_number)+'.pt')
 print("Experiment number: ", experiment_number)
